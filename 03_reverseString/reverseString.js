a2a4805a5affe0ac019c6a352e758cb734408e28
AahanMehta21 <mehta295@purdue.edu>
2025-01-25 18:07:16 -0500

    Added solution to exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        revStr += str[i];
+    }
+    return revStr;
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;

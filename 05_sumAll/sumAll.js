e158560f0cebe10a9d75b72b62189d2e924f7534
AahanMehta21 <mehta295@purdue.edu>
2025-01-25 19:40:04 -0500

    Added solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    let result = 0;
+    for (; num1 <= num2; num1++) {
+        result += num1;
+    }
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;

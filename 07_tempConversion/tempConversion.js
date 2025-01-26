a61a682a06bab21b33cc15f9840a04eacd39257d
AahanMehta21 <mehta295@purdue.edu>
2025-01-25 19:51:27 -0500

    Added solution to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(val) {
+  fullVal = (val - 32) * (5 / 9);
+  return Math.round(fullVal * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(val) {
+  fullVal = (val * 9) / 5 + 32;
+  return Math.round(fullVal * 10) / 10;
 };
 
 // Do not edit below this line
